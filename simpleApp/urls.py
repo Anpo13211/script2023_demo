23acb7ae5b1f9be709de71ab7f8a2d6824360485
Anpo13211 <126756334+Anpo13211@users.noreply.github.com>
2023-07-04 15:24:19 +0900

    Renew some html and css

--- simpleApp/urls.py
+++ simpleApp/urls.py
@@ -6,7 +6,9 @@ urlpatterns = [
     path('weather_form/', views.weather_form, name='weather_form'),
     path('weather_search/', views.weather_search, name='weather_search'),
     path('weather_search_by_coordinates/', views.weather_search_by_coordinates, name='weather_search_by_coordinates'),
+    path('navigate/', views.navigate, name='navigate'),
 ]
+
 
 
 
--- simpleApp/views.py
+++ simpleApp/views.py
@@ -23,45 +23,16 @@ params = {
 default_lat = '35.69'
 default_lon = '139.69'
 
-
-
-def weather_search_by_coordinates(request):
+def get_weather_data(request, by_coordinates=False):
     lat = request.GET.get('lat', default_lat)
     lon = request.GET.get('lon', default_lon)
-    current = async_to_sync(weather.search_weather_by_coordinates)(lat, lon)
-    forecast = async_to_sync(weather.search_forecast_by_coordinates)(lat, lon)
-
-    now = datetime.datetime.now()
-    if forecast:
-        for entry in forecast['list']:
-            entry['dt_txt'] = convert_utc_to_local(entry['dt_txt'])
-
-    if current['city'] == '堀ノ内':
-        current['city'] = 'Tokyo'
-        forecast['city']['name'] = 'Tokyo'
-
-    context = {
-        'forecast': forecast,
-        'current': current,
-        'current_time': now
-    }
-    return render(request, 'forecast.html', context)
-
-
-def convert_utc_to_local(utc_dt_str):
-    utc_dt = datetime.datetime.strptime(utc_dt_str, '%Y-%m-%d %H:%M:%S')
-    utc_dt = pytz.utc.localize(utc_dt)
-
-    local_tz = pytz.timezone('Asia/Tokyo')
-    local_dt = utc_dt.astimezone(local_tz)
-
-    return local_dt.strftime('%Y-%m-%d %H:%M:%S')
-
-def weather_view(request):
-    lat = request.GET.get('lat', default_lat)
-    lon = request.GET.get('lon', default_lon)
-    current = async_to_sync(weather.search_weather_by_coordinates)(lat, lon)
-    forecast = async_to_sync(weather.search_forecast_by_coordinates)(lat, lon)
+    if by_coordinates:
+        current = async_to_sync(weather.search_weather_by_coordinates)(lat, lon)
+        forecast = async_to_sync(weather.search_forecast_by_coordinates)(lat, lon)
+    else:
+        city = request.GET.get('city', CITY_NAME)
+        forecast = async_to_sync(weather.search_forecast)(city)
+        current = async_to_sync(weather.search_weather)(city)
 
     now = datetime.datetime.now()
 
@@ -78,26 +49,34 @@ def weather_view(request):
         'current': current,
         'current_time': now
     }
+    return context
+
+
+def weather_search_by_coordinates(request):
+    context = get_weather_data(request, by_coordinates=True)
     return render(request, 'forecast.html', context)
 
+
+def convert_utc_to_local(utc_dt_str):
+    utc_dt = datetime.datetime.strptime(utc_dt_str, '%Y-%m-%d %H:%M:%S')
+    utc_dt = pytz.utc.localize(utc_dt)
+
+    local_tz = pytz.timezone('Asia/Tokyo')
+    local_dt = utc_dt.astimezone(local_tz)
+
+    return local_dt.strftime('%Y-%m-%d %H:%M:%S')
+
+def weather_view(request):
+    context = get_weather_data(request)
+    return render(request, 'forecast.html', context)
 
 def weather_form(request):
     return render(request, 'search_form.html')
 
 def weather_search(request):
-    city = request.GET.get('city', CITY_NAME)
-    forecast = async_to_sync(weather.search_forecast)(city)
-    current = async_to_sync(weather.search_weather)(city)
-
-    now = datetime.datetime.now()
-    if forecast:
-        for entry in forecast['list']:
-            entry['dt_txt'] = convert_utc_to_local(entry['dt_txt'])
-
-    context = {
-        'city': city,
-        'forecast': forecast,
-        'current': current,
-        'current_time': now
-    }
+    context = get_weather_data(request)
     return render(request, 'search.html', context)
+
+def navigate(request):
+    return render(request, 'navigate.html')
+
