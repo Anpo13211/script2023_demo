dd70fed1bd44b104a875cc7ef3cb3fe631379828
Anpo13211 <126756334+Anpo13211@users.noreply.github.com>
2023-07-07 19:42:28 +0900

    Refine some css designs and Javascript.

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-!vz9om(d&9lad3i=6+_f55^n6&vmiiq$uv+7eo2209%4nh+*6%"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app"]
 
